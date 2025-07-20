c3a880e5c3dd8b05d6671196c0e7c5b1eb22ec90
Abirami M <121538034+Abirami04M@users.noreply.github.com>
2025-07-20 12:45:20 +0530

    Update train_model.py

--- phishing-detection-ci/src/train_model.py
+++ phishing-detection-ci/src/train_model.py
@@ -5,8 +5,8 @@ from sklearn.metrics import accuracy_score, classification_report
 from data_preprocess import preprocess_data
 
 # Load and preprocess training & test data
-X_train, y_train = preprocess_data('phishing-detection-cicid/data/training.csv')
-X_test, y_test = preprocess_data('phishing-detection-cicid/data/testing.csv')
+X_train, y_train = preprocess_data('phishing-detection-ci/data/training.csv')
+X_test, y_test = preprocess_data('phishing-detection-ci/data/testing.csv')
 
 # Train model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -18,5 +18,5 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
 
 # Save base model
-joblib.dump(model, 'phishing-detection-cicid/models/base_model.pkl')
+joblib.dump(model, 'phishing-detection-ci/models/base_model.pkl')
 print("✅ Model saved as base_model.pkl")
